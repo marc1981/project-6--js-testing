6245a21fe5f6f3c114222c4f34c765f637c16ab7
Marc Anderson <marc_anderson_1981@yahoo.com>
2016-01-08 13:43:16 -0600

    Live version

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# Project Overview
+
+The purpose of this project was to become familiar with the Behavior-Driven Development (BDD) framework, [Jasmine](http://jasmine.github.io/). A web-based, RSS feed-reader was subjected to a series of pass-fail test suites in order to ensure its functionality. The tests evaluated the underlying business logic of the application as well as the event handling and DOM manipulation.
+
+* A live version of this application can be viewed here: [Project-6 Feed Reader](http://marc1981.github.io/project-6--js-testing)
+
+# How was this project completed? - The following steps were taken in order to complete this project.
+
+1. Download the [required project assets](http://github.com/udacity/frontend-nanodegree-feedreader).
+2. Review the functionality of the application within your browser.
+3. Explore the application's HTML (*./index.html*), CSS (*./css/style.css*) and JavaScript (*./js/app.js*) to gain an understanding of how it works.
+4. Explore the Jasmine spec file in *./jasmine/spec/feedreader.js*
+5. Edit the allFeeds variable in *./js/app.js* to make the provided test fail and see how Jasmine visualizes this failure in your application.
+6. Return the allFeeds variable to a passing state.
+7. Write a test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.
+8. Write a test that loops through each feed in the allFeeds object and ensures it has a name defined and that the name is not empty.
+9. Write a new test suite named "The menu".
+10. Write a test that ensures the menu element is hidden by default. You'll have to analyze the HTML and the CSS to determine how we're performing the hiding/showing of the menu element.
+11. Write a test that ensures the menu changes visibility when the menu icon is clicked. This test should have two expectations: does the menu display when clicked and does it hide when clicked again.
+12. Write a test that ensures when the loadFeed function is called and completes its work, there is at least a single .entry element within the .feed container. Remember, loadFeed() is asynchronous so this test wil require the use of Jasmine's beforeEach and asynchronous done() function.
+13. Write a test that ensures when a new feed is loaded by the loadFeed function that the content actually changes. Remember, loadFeed() is asynchronous.
+14. When complete - all of your tests should pass.
+
+## Additional steps taken
+
+1. Two matchers that were not available in the default Jasmine framework were created.
+	* Additional Jasmine matchers `.toHaveClass` and `.toHaveCss` were added based off of code from: https://github.com/velesin/jasmine-jquery/blob/master/lib/jasmine-jquery.js
+2. An additional test suite was added to check the functionality of a series of pop-up windows.
+	* CSS-popup provided by: [Patrick Burt](http://www.pat-burt.com/web-development/how-to-do-a-css-popup-without-opening-a-new-window/)
+3. As mentioned, pop-up windows were added to provide the user a screenshot of what happens when the app fails a particular test suite.
+4. Using Bootstrap causes formating issues, so a modified grid system was created using unique class names.
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,185 @@
+/* feedreader.js
+    Comments updated to reflect changes made while completing project.
+ */
+
+$(function() {
+        
+    beforeEach(function () {
+        jasmine.addMatchers({
+            //Add Jasmine Matchers for Class and Css Styles.
+            //Thanks to https://github.com/velesin/jasmine-jquery/blob/master/lib/jasmine-jquery.js
+            toHaveClass: function () {
+                return {
+                    compare: function (actual, className) {
+                        return { pass: $(actual).hasClass(className) }
+                    }
+                }
+            },
+
+            toHaveCss: function () {
+                return {
+                  compare: function (actual, css) {
+                    var stripCharsRegex = /[\s;\"\']/g
+                    for (var prop in css) {
+                        var value = css[prop];
+                        if ((value === 'auto') && ($(actual).get(0).style[prop] === 'auto')) continue
+                            var actualStripped = $(actual).css(prop).replace(stripCharsRegex, '')
+                            var valueStripped = value.replace(stripCharsRegex, '')
+                        if (actualStripped !== valueStripped) return { pass: false }
+                        }
+                    return { pass: true }
+                    }
+                }
+            }
+            
+        }) 
+
+    });
+    
+    describe('RSS Feeds', function() {
+        /* This is our first test - it tests to make sure that the
+         * allFeeds variable has been defined and that it is not
+         * empty. Experiment with this before you get started on
+         * the rest of this project. What happens when you change
+         * allFeeds in app.js to be an empty array and refresh the
+         * page?
+         */
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+
+        /* A test that loops through each feed
+         * in the allFeeds object and ensures it has a URL defined
+         * and that the URL is not empty.
+         */
+         it('defines url and is not empty', function(){
+            for(var i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].url).toBeDefined();
+            /*
+            *The following line states that the expectation
+            *is that the url will be present and not be an empty string.
+            */
+                expect(allFeeds[i].url).not.toBe(0 || '');
+            /*
+            *The following line ensures that url has met the minimum requirements
+            *for a functional feed, namely it contains 'http' and 'feed'. Not a
+            *guarantee, but better than nothing.
+            */
+                expect(allFeeds[i].url).toContain('http' && 'feed');
+            }
+         });
+
+        /* A test that loops through each feed
+         * in the allFeeds object and ensures it has a name defined
+         * and that the name is not empty.
+         */
+         it('defines name and is not empty', function(){
+            for(var i = 0; i < allFeeds.length; i++){
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name).not.toBe(0 || '');
+            }
+         });
+    });
+
+
+    /* A test suite named "The Menu" */
+    describe('The Menu', function(){ 
+        /* A test that ensures the menu element is
+         * hidden by default.
+         */
+        var menu = $('body');
+        var menuSelect = $('.menu-icon-link');
+
+        it('hides menu by default', function(){          
+            expect(menu).toHaveClass("menu-hidden");
+        });
+
+         /* A test that ensures the menu changes
+          * visibility when the menu icon is clicked. This test
+          * has two expectations: does the menu display when
+          * clicked and does it hide when clicked again.
+          */
+        it('changes menu visibility when clicked', function(){
+            //click the menu icon to show the menu
+            menuSelect.trigger('click');
+            //check if it is visible
+            expect(menu).not.toHaveClass("menu-hidden");
+            //click the icon again
+            menuSelect.trigger('click');
+            //check if it is hidden
+            expect(menu).toHaveClass("menu-hidden");
+
+        });
+
+    });
+
+    /*Test suite to make sure pop-up images respond to clicks*/
+    describe('Screenshot row', function(){
+        var screenshot = $('.pop');
+        var imageSelect = $('.imagePop');
+
+        it('pop up image on click', function(){
+            //click the image-icon to show the larger image
+            imageSelect.trigger('click');
+            //check if it is visible
+            expect(screenshot).toHaveCss({"display":"block"});
+            //click the icon again
+            imageSelect.trigger('click');
+            //check if it is hidden
+            expect(screenshot).toHaveCss({"display":"none"});
+        });
+
+    });
+
+    /* A test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
+
+        //Loops through all feeds starting at index 0.
+        for (var i = 0; i<allFeeds.length-1; i++){
+            beforeEach(function(done){
+                loadFeed(i, function(){
+                    done();
+                });
+            });
+
+            //Ensures that each feed has at least one element.
+            it('has at least one entry within feed', function(done){
+                expect($('.feed .entry').length).toBeGreaterThan(0);
+                done();
+            });
+        }
+
+    });
+
+    /* A test suite named "New Feed Selection" */
+    describe('New Feed Selection', function(){
+     /* A test that ensures when a new feed is loaded
+      * by the loadFeed function that the content actually changes.
+     */
+
+        var header, entry;
+
+        beforeEach(function(done) { 
+            loadFeed(0, function() {
+                header = $('h1.header-title').text();
+                entry = $('.feed .entry h2').text();
+                loadFeed(1, function() {
+                    done();
+                });
+            });
+        });
+
+        //Checks that header updates.
+        it('updates header title', function() {
+            expect($('h1.header-title').text()).not.toBe(header);
+        });
+
+        //Checks that post title updates.
+        it('updates entry', function() {
+            expect($('.feed .entry h2').text()).not.toBe(entry);    
+        });
+    });
+
+}());
